e419ec60d97739a38be06f0a109d31164ab23fb2
SariPaavilainen <sari.paavilainen@iki.fi>
2018-08-21 16:23:40 +0300

    Question submit-koodia siirtelin main pagelta question pagelle

--- QAiku/QAiku/QuestionPage.xaml.cs
+++ QAiku/QAiku/QuestionPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,33 @@ using Xamarin.Forms.Xaml;
 
 namespace QAiku
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class QuestionPage : ContentPage
-	{
-		public QuestionPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuestionPage : ContentPage
+    {
+        public QuestionPage ()
+        {
+            InitializeComponent ();
+        }
+        HttpClient httpClient = new HttpClient();
+
+        private async void SendQuestionButton_Clicked(object sender, EventArgs e)
+        {
+            var msg = "jepujee";
+
+            
+
+            try
+            {
+                string Url = "http://qaiku.azurewebsites.net/api/messages/post";
+                //var content = JsonConvert.SerializeObject(msg);
+                var response = await httpClient.PostAsync(Url, new StringContent(msg, Encoding.UTF8, "application/json"));
+                Console.WriteLine(response.Content.ReadAsStringAsync());
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
 }
